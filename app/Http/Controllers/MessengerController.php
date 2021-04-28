813cf719088a5f5cc45baf9846a36f0cc97f1437
dam.trieu.van.the <dam.trieu.van.the@sun-asterisk.com>
2021-04-28 08:19:25 +0700

    Messenger

---
+++ app/Http/Controllers/MessengerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class MessengerController extends Controller
+{
+    use AuthorizesRequests;
+
+    /**
+     * Login.
+     */
+    public function index()
+    {
+
+    }
+}
